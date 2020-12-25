cb43dbef2b695191483d8815d41eed508e1090d7
maaislam <maaislam@outlook.com>
2020-12-25 15:55:18 +0600

    app refactor

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -2,12 +2,13 @@
 import axios from 'axios';
 
 
-
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+console.log("🚀 ~ file: youtube.js ~ line 6 ~ API_KEY", API_KEY)
 export default axios.create({
 
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params:{
-        key: 'AIzaSyAa2TjKlnKFDRT2EpfxDEsb_JB7GO-JldY',
+        key: API_KEY,
         part: "snippet",
         maxResults: 10
     }
